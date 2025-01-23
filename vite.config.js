90429faaddce05a288b60774ddd8030ec57c2907
Can <cannmatik@gmail.com>
2025-01-23 13:02:13 +0300

    sa

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@api": path.resolve(__dirname, "./src/api/index.js"),
-      "@assets": path.resolve(__dirname, "./src/assets/index.js"),
-      "@icons": path.resolve(__dirname, "./src/assets/icons/index.js"),
-      "@components": path.resolve(__dirname, "./src/components/index.js"),
-      "@routes": path.resolve(__dirname, "./src/routes"),
-      "@enums": path.resolve(__dirname, "./src/enums/index.js"),
-      "@hooks": path.resolve(__dirname, "./src/hooks/index.js"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@utils": path.resolve(__dirname, "./src/utils/index.js"),
-      "@validations": path.resolve(__dirname, "./src/validations"),
+      '@api': path.resolve(__dirname, './src/api/index.js'),
+      '@assets': path.resolve(__dirname, './src/assets/index.js'),
+      '@icons': path.resolve(__dirname, './src/assets/icons/index.js'),
+      '@components': path.resolve(__dirname, './src/components/index.js'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@enums': path.resolve(__dirname, './src/enums/index.js'),
+      '@hooks': path.resolve(__dirname, './src/hooks/index.js'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@utils': path.resolve(__dirname, './src/utils/index.js'),
+      '@validations': path.resolve(__dirname, './src/validations'),
     },
   },
   css: {
@@ -27,8 +27,9 @@ export default defineConfig({
       },
     },
   },
-  envPrefix: "IlacimNerede_",
+  envPrefix: 'IlacimNerede_',
+  base: '/ilacimnerede/', // Bu satırı ekleyin: Base URL'yi burada belirtin
   server: {
-    historyApiFallback: true,  // Bu satırı ekleyin
+    historyApiFallback: true,
   },
 });

0e2b57ed5bfde3308100365be212c62c5fc6aa7a
Can <cannmatik@gmail.com>
2025-01-30 18:41:01 +0300

    loading

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -39,7 +39,7 @@ function Layout() {
           }
           return Math.min(prevProgress + Math.random() * 10, 100);
         });
-      }, 200);
+      }, 100);
 
       return () => clearInterval(progressInterval);
     }

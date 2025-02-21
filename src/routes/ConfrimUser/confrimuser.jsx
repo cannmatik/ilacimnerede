63310753d44dd9b119e02b80dc008816f4d92b72
Can <cannmatik@gmail.com>
2025-02-21 14:23:06 +0300

    confrimuser

--- src/routes/ConfrimUser/confrimuser.jsx
+++ src/routes/ConfrimUser/confrimuser.jsx
@@ -10,9 +10,7 @@ const ConfirmUser = () => {
 
   useEffect(() => {
     const token_hash = searchParams.get('token_hash');
-    // Eğer type parametresi gönderilmezse, varsayılan olarak 'email' kullanılır.
     const type = searchParams.get('type') || 'email';
-    const redirect_to = searchParams.get('redirect_to');
 
     if (token_hash && type) {
       supabase.auth.verifyOtp({ token_hash, type })
@@ -21,13 +19,12 @@ const ConfirmUser = () => {
             setMessage(`Doğrulama sırasında hata: ${error.message}`);
             setStatus('error');
           } else {
-            setMessage('E‑posta doğrulaması başarılı. Artık giriş yapabilirsiniz.');
+            setMessage('E‑posta doğrulaması başarılı.');
             setStatus('success');
-            if (redirect_to) {
-              setTimeout(() => {
-                window.location.href = redirect_to;
-              }, 2000);
-            }
+            // 2 saniye sonra sekmeyi kapatmayı deniyoruz.
+            setTimeout(() => {
+              window.close();
+            }, 2000);
           }
         });
     } else {
